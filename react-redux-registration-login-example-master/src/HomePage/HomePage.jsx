6bfe15e858c2932314255f90a8381906e06f34c7
Tafdav <46773345+Tafdav@users.noreply.github.com>
2019-02-01 12:42:26 +0200

    Added proper routing for wrong urls

--- react-redux-registration-login-example-master/src/HomePage/HomePage.jsx
+++ react-redux-registration-login-example-master/src/HomePage/HomePage.jsx
@@ -17,27 +17,30 @@ class HomePage extends React.Component {
     const { user, users } = this.props;
     return (
       <div className="mainSection">
-      <h1>Hi Tired Programmers Here!</h1>
+        <h1>Hi Tired Programmers Here!</h1>
         <img src="tired.jpg" alt="Tired Coders"/>
         <h1>Hi {user.firstName}!</h1>
         <p>You're logged in with React!!</p>
         <h3>All registered users:</h3>
         {users.loading && <em>Loading users...</em>}
         {users.error && <span className="text-danger">ERROR: {users.error}</span>}
-          {users.items && 
+        {
+          users.items && 
             <ul>
-              {users.items.map((user, index) => 
-                <li key={user.id}>
-                  {user.firstName + " " + user.lastName}
-                  {user.deleting ? <em> - Deleting...</em> 
-                  : user.deleteError ?  <span className="text-danger">{" "}- ERROR: {user.deleteError} </span> : 
-                    <span> - <a onClick={this.handleDeleteUser(user.id)}>Delete</a></span> 
+              {
+                users.items.map((user, index) => 
+                  <li key={user.id}>
+                    {user.firstName + " " + user.lastName}
+                    {
+                      user.deleting ? <em> - Deleting...</em> 
+                      : user.deleteError ?  <span className="text-danger">{" "}- ERROR: {user.deleteError} </span> : 
+                      <span> - <a onClick={this.handleDeleteUser(user.id)}>Delete</a></span> 
                     }
-                </li>
-              )}
+                  </li>
+                )
+              }
             </ul>
-          }
-        
+        }
         <p>
           <Link to="/login">Logout</Link>
         </p>
